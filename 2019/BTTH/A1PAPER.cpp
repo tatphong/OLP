88399167426de5f02209ecc213666f0614dd5ee1
tatphong <50413495+tatphong@users.noreply.github.com>
2019-09-03 10:33:54 +0700

    update a1paper

--- 2019/BTTH/A1PAPER.cpp
+++ 2019/BTTH/A1PAPER.cpp
@@ -16,35 +16,44 @@ void timdodai()
 int main()
 {
     cin>>n;
-//    cout<<double(pow(2,-3*1.0/4))<<endl;
     int x;
-    for (int i=2;i<=n;++i)
+    for (int i=2;i<=n;++i) //cin here
     {
         cin>>x;
         a[i]=x;
     }
-    if (a[n]%2!=0) --a[n];
-//    for (int i=n;i>1;--i)
-//    {
-//        b[i-1]=a[i-1]+a[i]/2+b[i]/2;
-//        b[i]%=2;
-//    }
+//    if (a[n]%2!=0) --a[n];
+
+
+    //solution
+    long long s=0;
 //    for (int i=2;i<=n;++i)
 //    {
-//        a[i]-=b[i];
-//        cout<<a[i]<<" ";
+//        s+=pow(2,30-i-1)*a[i];
+//        if (s>=a1s) ;
 //    }
-//    cout<<endl<<endl;
-//    for (int i=1;i<=n;++i)
-//        cout<<a[i]<<" "<<b[i]<<endl;
-//    if (b[1]==0) {cout<<"impossible";return 0;}
-    long long s=0,a1s=pow(2,29);
+//    if (s<a1s) {cout<<"impossible";return 0;}
+
+    //check
+    int temp=2,check=0;
     for (int i=2;i<=n;++i)
     {
-        s+=pow(2,30-i-1)*a[i];
-        if (s>=a1s) ;
+        if (temp>a[i])
+        {
+            temp-=a[i];
+            temp*=2;
+        }
+        else
+        {
+            a[i]=temp;
+            n=i;
+            check=1;
+        }
     }
-    if (s<a1s) {cout<<"impossible";return 0;}
+//    for (int i=2;i<=n;++i)
+//        cout<<a[i]<<" ";
+    if (!check) {cout<<"impossible";return 0;}
+    //solve
     timdodai();
     long double cv=0,a1=(d[2].first+d[2].second*2)*2;
     for (int i=2;i<=n;++i)
