92f9b881872a94d40de2a1d42474d92fc99f1461
tatphong <50413495+tatphong@users.noreply.github.com>
2019-10-08 17:02:50 +0700

    Update islands.cpp

--- 2019/islands.cpp
+++ 2019/islands.cpp
@@ -1,31 +1,35 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> //AC 100
 using namespace std;
-typedef pair<string,int>si;
-int m,n;
+typedef pair<char,int>si;
+int m,n,dem=0;
 si a[100][100];
 void loang(int i,int j)
 {
-    if (a[i+1][j].first=="C"||a[i+1][j].first=="L") {a[i+1][j].second=1;loang(i+1,j);}
-    if (a[i-1][j].first=="C"||a[i-1][j].first=="L") {a[i-1][j].second=1;loang(i-1,j);}
-    if (a[i][j+1].first=="C"||a[i][j+1].first=="L") {a[i][j+1].second=1;loang(i,j+1);}
-    if (a[i][j-1].first=="C"||a[i][j-1].first=="L") {a[i][j-1].second=1;loang(i,j-1);}
+    if (i+1<m && a[i+1][j].second==0 && (a[i+1][j].first=='C'||a[i+1][j].first=='L')) {a[i+1][j].second=1;loang(i+1,j);}
+    if (i-1>=0 && a[i-1][j].second==0 && (a[i-1][j].first=='C'||a[i-1][j].first=='L')) {a[i-1][j].second=1;loang(i-1,j);}
+    if (j+1<n && a[i][j+1].second==0 && (a[i][j+1].first=='C'||a[i][j+1].first=='L')) {a[i][j+1].second=1;loang(i,j+1);}
+    if (j-1>=0 && a[i][j-1].second==0 && (a[i][j-1].first=='C'||a[i][j-1].first=='L')) {a[i][j-1].second=1;loang(i,j-1);}
 }
 int main()
 {
     while (cin>>m>>n)
     {
+        dem=0;
         for (int i=0;i<m;++i)
         for (int j=0;j<n;++j)
             cin>>a[i][j].first;
         for (int i=0;i<m;++i)
         for (int j=0;j<n;++j)
         {
-            if (a[i][j].first=="L")
+            if (a[i][j].first=='L' && a[i][j].second!=1)
             {
                 a[i][j].second=1;
                 loang(i,j);
+                ++dem;
             }
         }
+        cout<<dem;
     }
     return 0;
 }
+
