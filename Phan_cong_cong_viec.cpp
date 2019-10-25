eb149ee67ca7d660cb1ef49a89548d1e5b15ae01
tatphong <50413495+tatphong@users.noreply.github.com>
2019-10-25 14:07:19 +0700

    Create Phan_cong_cong_viec.cpp

---
+++ Phan_cong_cong_viec.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,a[100000],cv[1000];
+int main()
+{
+    freopen("GTS1.INP","r",stdin);
+    cin>>n>>m;
+    for (int i=0;i<m;++i)
+        cin>>a[i];
+
+    //solve
+    sort(a,a+m,greater<int>());
+    //note: cv[x][0] is sum of time to do all jobs
+    int i=0;
+    while(i<m)
+    {
+        for (int j=0;j<n;++j)
+            cv[j]+=a[i++];
+        sort(cv,cv+n);
+    }
+    cout<<cv[n-1];
+    return 0;
+}
